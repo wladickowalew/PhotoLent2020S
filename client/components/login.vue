10d130a65739fffd46da64026ad6079e19709b0b
kowlad <wladickowalew@gmail.com>
2020-08-03 16:39:26 +0300

    add register and login forms

---
+++ client/components/login.vue
@@ -0,0 +1,46 @@
+<style scoped>
+	#log{
+		background: blue;
+		width: 100%;
+		border-color: grey;
+	}
+	input, #log{
+		box-shadow: 0 0 10px rgba(0,0,0,0.5);
+	}
+	form{
+		width: 700px;
+	}
+	*{
+		font-size: 25px;
+	}
+	a{
+		font-size: 17px;
+	}
+</style>
+
+<template>
+	<div>
+		<form id="login_form">
+		  <div class="form-group">
+		    <label for="login">Логин</label>
+		    <input type="text" class="form-control" id="login" placeholder="Введите Логин">
+		  </div>
+		  <div class="form-group">
+		    <label for="password">Пароль</label>
+		    <input type="password" class="form-control" id="password" placeholder="Введите пароль">
+		  </div>
+		  <button type="submit" class="btn btn-primary" id="log" v-on:click="login">Войти!</button>
+		  <center><a href="http://localhost:8080/#/register" class="form-link" >Регистрация</a></center>
+		</form>
+	</div>
+</template>
+
+<script>
+	module.exports={
+		methods:{
+			login: function() {
+				console.log("login");
+			}
+		}
+	}
+</script>
---
+++ client/components/register.vue
@@ -0,0 +1,46 @@
+<style>
+	#reg{
+		background: red;
+		width: 100%;
+		border-color: grey;
+	}
+	input, #reg{
+		box-shadow: 0 0 10px rgba(0,0,0,0.5);
+	}
+	form{
+		width: 700px;
+	}
+	*{
+		font-size: 25px;
+	}
+</style>
+
+<template>
+	<div>
+		<form id="reg_form">
+		  <div class="form-group" >
+		    <label for="login">Логин</label>
+		    <input type="text" class="form-control" id="login" placeholder="Введите Логин">
+		  </div>
+		  <div class="form-group">
+		    <label for="password">Пароль</label>
+		    <input type="password" class="form-control" id="password" placeholder="Введите пароль">
+		  </div>
+		  <div class="form-group">
+		    <label for="password_repeat">Повтор пароля</label>
+		    <input type="password" class="form-control" id="password_repeat" placeholder="Повторите пароль">
+		  </div>
+		  <button type="submit" class="btn btn-primary" id="reg" v-on:click="register">Зарегистрироваться!</button>
+		</form>
+	</div>
+</template>
+
+<script>
+	module.exports={
+		methods:{
+			register: function() {
+				console.log("register");
+			}
+		}
+	}
+</script>

b8ee42ad64bc8febd68fd4e7dccf192ef981dafa
kowlad <wladickowalew@gmail.com>
2020-08-05 17:38:15 +0300

    final

--- client/components/feed.vue
+++ client/components/feed.vue
@@ -1,5 +1,37 @@
+<style scoped>
+	*{
+		font-size: 25px;
+	}
+	#photolenta{
+		margin-top: 500px;
+		background-color: green;
+	}
+</style>
+
 <template>
 	<div>
-		<p>Здесь будут новости!</p>
+		<header-m></header-m>
+		<photo-card v-for="record in records" v-bind:record="record"></photo-card>
 	</div>
 </template>
+
+<script>
+	let Header = require("./header.vue");
+	let PhotoCard = require("./photoCard.vue");
+	module.exports={
+		components:{
+			headerM: Header,
+			photoCard: PhotoCard
+		},
+		data: function() {
+			return {records: []}
+		},
+		mounted: function(){
+			this.$http.get("/feed", {bearer: true}).then(
+				function(res){
+					console.log(res.body);
+					this.records = res.body;
+				});
+		}
+	}
+</script>
---
+++ client/components/photoCard.vue
@@ -0,0 +1,22 @@
+<style scoped>
+	div{
+		background: grey;
+		margin: 20px;
+	}
+	a{
+		font-size: 17px;
+	}
+</style>
+
+<template>
+	<div class="card" style="width: 18rem;">
+		<div class="card-header">
+			<h5 class="card-title">{{record.user}}</h5>
+		</div>
+		<img v-bind:src="'./data/' + record.file" class="card-img-top">
+	</div>
+</template>
+
+<script>
+	module.exports = {props: ["record"]};
+</script>
